b073138379d7c48e6d8cdd6f536c9c2452b129ed
Erik <happy@MacBook-Pro.local>
2020-08-19 08:27:55 +0800

    Lesson 6

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function guest_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        
+        $thread = factory('App\Thread')->make();
+
+        $this->post('threads', $thread->toArray());
+
+    }
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        // Given we have a signed in user
+        $this->actingAs(factory('App\User')->create());
+
+        // When we hit the endpoint to create a new thread
+        $thread = factory('App\Thread')->make();  // ehleed not persist tul make or raw gene.
+
+        $this->post('threads', $thread->toArray());
+
+        // Then we visit the thread page
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+   }
+}
