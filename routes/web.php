fbd34b46ced0da4c176763c4c44b8a1a110d80fc
Erik <happy@MacBook-Pro.local>
2020-08-22 17:10:14 +0800

    Lesson 18

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
 
 //Route::resource('threads', 'ThreadsController');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
 
 Auth::routes();
 
