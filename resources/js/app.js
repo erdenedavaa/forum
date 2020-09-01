134e5a300d93f1de363d0f2da276b6b9da0328e5
Erik <happy@MacBook-Pro.local>
2020-09-01 22:43:03 +0800

    Lesson 29

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,13 +13,19 @@ window.Vue = require('vue');
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/Flash.vue -> <example-component></example-component>
  */
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('flash', require('./components/Flash.vue').default);
+
+window.events = new Vue();
+
+window.flash = function (message) {
+    window.events.$emit('flash', message);
+} // flash('my new flash message')
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
