e19dd096301ce4d997d7f0c70499282a4f5f2038
erdenedavaa <erdenedavaa@users.noreply.github.com>
2020-10-04 13:43:01 +0800

    Lesson 94

--- app/Thread.php
+++ app/Thread.php
@@ -5,10 +5,11 @@ namespace App;
 use App\Events\ThreadReceivedNewReply;
 use App\Filters\ThreadFilters;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Thread extends Model
 {
-    use RecordsActivity;
+    use RecordsActivity, Searchable;
 
     protected $guarded = [];
 
